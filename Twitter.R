efe80e33cb8651a615567a5898af05d7c4aeb9d4
GusChac <gustavochac@hotmail.com>
2021-11-10 11:09:16 -0300

    Update Twitter.R

--- Twitter.R
+++ Twitter.R
@@ -1,3 +1,5 @@
+# INSTALACION DE PAQUETES NECESARIOS PARA EL PROYECTO ----
+
 install.packages("twitteR")
 install.packages("tm")
 install.packages("syuzhet")
@@ -6,11 +8,17 @@ install.packages("wordcloud")
 install.packages("ggplot2")
 install.packages("topicmodels")
 
+ 
+
+# CREDENCIALES DE LA API TWITTER ----
+
 API_key <- "HIDE"
 API_key_secret <- "HIDE"
 Token <- "HIDE"
 Access_token <- "HIDE"
 Access_token_secret <- "HIDE"
+
+# INVOCACIONES DE LIBRERIAS NECESARIAS ----
 
 library(twitteR)
 library(tm)
@@ -20,23 +28,89 @@ library(wordcloud)
 library(ggplot2)
 library(topicmodels)
 
-setup_twitter_oauth(API_key,API_key_secret,Access_token,Access_token_secret)
-?searchTwitter
-tweet_corpus <- searchTwitter('#RavenCoin', n=100000 ,lang = 'en', since = '2021-10-01', until = '2021-10-20')
-tweet_corpus <- strip_retweets(tweet_corpus)
-tweet_df <- twListToDF(tweet_corpus)
-tweets <- iconv(tweet_df$text, to = 'ASCII')
+# OBTENCION DE DATOS TWITTER ----
 
+# LOGIN API TWITTER
+setup_twitter_oauth(API_key,API_key_secret,Access_token,Access_token_secret)
+
+# IMPORTACION DE DATOS
+tweet_corpus <- searchTwitter('#RavenCoin', n=100000 ,lang = 'en', since = '2021-11-01', until = '2021-11-10')
+
+#EXPORTACION DE DATOS A CSV (ESTE PASO ES MUY IMPORTANTE PORQUE LAS CONSULTAS EN TWITTER SON LIMITADAS)
+write.csv(twListToDF(tweet_corpus),'datoscsv')
+write.csv2(twListToDF(tweet_corpus),'datoscsv2')
+
+# ELIMINACIN DE RETWEETS
+# SE PUEDE UTILIZAR SI SE QUIEREN ANALIZAR UNICAMENTE LOS GENERADORES DE OPINION
+tweet_corpus <- strip_retweets(tweet_corpus)
+
+# TRANSFORMACION A DATA FRAME
+tweet_df <- twListToDF(tweet_corpus)
+
+# VECTOR DE COMENTARIOS PARA ANALISIS DE SENSIBILIDAD
+tweets <- iconv(tweet_df$text)
+
+# GENERACION DE ANALISIS DE SENTIMIENTO ----
+
+# REALIZO 5 ANALISIS DISTINTOS
 sent_syuzhet <- get_sentiment(tweets, method = 'syuzhet')
 sent_bing <- get_sentiment(tweets, method = 'bing')
 sent_afinn <- get_sentiment(tweets, method = 'afinn')
 sent_nrc <- get_sentiment(tweets, method = 'nrc')
 nrc_data <- get_nrc_sentiment(tweets)
 
-
-tweet_df <- cbind(tweet_df, sent_syuzhet, sent_bing,
+# AGREGO EL ANALISIS AL DATA FRAME
+tweet_df_sa <- cbind(tweet_df, sent_syuzhet, sent_bing,
                   sent_afinn, sent_nrc, nrc_data)
 
+# EXPORTO A CSV EL NUEVO DATA FRAME
+write.csv(tweet_df_sa,"datos.sa.csv")
+write.csv2(tweet_df_sa,"datos.sa.csv2")
+
+#### ANALISIS DE COMPORTAMIENTO (2 HORAS)
+
+hora.inicial <- rep(as.POSIXct('2021-11-01 00:00:00'),120)
+for(i in 1:120) {
+  hora.inicial[i] <- hora.inicial[i]+((i-1)*7200)
+}
+hora.final <- rep(as.POSIXct('2021-11-01 00:00:00'),120)
+for(i in 1:120) {
+  hora.final[i] <- hora.final[i]+(i*7200)
+}
+
+a<-rep(0,120)
+for (j in 1:120) {
+  for (i in 1:length(tweet_df_sa$created)) {
+    if (tweet_df_sa$created[i] >= hora.inicial[j] & tweet_df_sa$created[i] < hora.final[j]) {
+      a[j]<-a[j]+1
+    }
+  }
+}
+
+b<-rep(0,120)
+for (j in 1:120) {
+  for (i in 1:length(tweet_df_sa$created)) {
+    if (tweet_df_sa$created[i] >= hora.inicial[j] & tweet_df_sa$created[i] < hora.final[j]) {
+      b[j]<-b[j]+tweet_df_sa$sent_syuzhet[i]
+    }
+  }
+}
+b
+c <- rep(0,120)
+for(i in 1:length(b)) {
+  c[i] <- b[i]/a[i]
+}
+c
+plot(a,type = "l")
+getwd()
+
+datosbarplot <- data.frame(hora.inicial,hora.final,a,b,c)
+
+datosbarplot<- datosbarplot[6:107,]
+
+datosbarplot$hora.final <- datosbarplot$hora.final+60*60*2
+
+setwd("/Users/gustavochac/Documents/GitHub/Text Minning Twitter")
 mean(sent_bing)
 plot(sent_syuzhet)
 hist(sent_syuzhet)
@@ -45,7 +119,7 @@ hist(sent_nrc)
 hist(sent_afinn)
 
 count.positive <- 0
-for (i in 1:854) {
+for (i in 1:6990) {
   if(sent_syuzhet[i]>0) {
     count.positive <- count.positive +1
   }
@@ -53,7 +127,7 @@ for (i in 1:854) {
 count.positive
 
 count.neutral <- 0
-for (i in 1:854) {
+for (i in 1:6990) {
   if(sent_syuzhet[i]==0) {
     count.neutral <- count.neutral +1
   }
@@ -61,7 +135,7 @@ for (i in 1:854) {
 count.neutral
 
 count.negative <- 0
-for (i in 1:854) {
+for (i in 1:6990) {
   if(sent_syuzhet[i]<0) {
     count.negative <- count.negative +1
   }
